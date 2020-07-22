159485c4d73d7f1b38574d67e5ee5cffebfd4918
YukiSasaki <tokidrill@gmail.com>
2020-07-22 23:55:35 +0900

    feat: replace functional component

--- src/index.tsx
+++ src/index.tsx
@@ -7,28 +7,12 @@ interface SquareProps {
   onClick: () => void
 }
 
-interface SquareState {
-  value: string
-}
-
-class Square extends React.Component<SquareProps, SquareState> {
-  constructor(props: SquareProps) {
-    super(props)
-    this.state = {
-      value: ""
-    }
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    )
-  }
+const Square = (props: SquareProps) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 interface BoardProps {
